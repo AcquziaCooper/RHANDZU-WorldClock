f5672a11a73534a0bb0e344c2a2a71e81234a53b
AcquziaCooper <lungiekunene@gmail.com>
2024-08-23 10:52:26 +0200

    Added multiple Time Zone

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,40 @@
 setInterval(function () {
   let joburgElement = document.querySelector("#joburg");
-  let joburgDateElement = joburgElement.querySelector(".date");
-  let joburgTimeElement = joburgElement.querySelector(".time");
-  joburgDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-  joburgTimeElement.innerHTML = moment()
-    .tz("Africa/Johannesburg")
-    .format("H:mm:s");
+  if (joburgElement) {
+    let joburgDateElement = joburgElement.querySelector(".date");
+    let joburgTimeElement = joburgElement.querySelector(".time");
+    joburgDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+    joburgTimeElement.innerHTML = moment()
+      .tz("Africa/Johannesburg")
+      .format("H:mm:s");
+  }
 
   let mauritiusElement = document.querySelector("#mauritius");
-  let mauritiusDateElement = mauritiusElement.querySelector(".date");
-  let mauritiusTimeElement = mauritiusElement.querySelector(".time");
-  mauritiusDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-  mauritiusTimeElement.innerHTML = moment()
-    .tz("Indian/Mauritius")
-    .format("H:mm:s");
+  if (mauritiusElement) {
+    let mauritiusDateElement = mauritiusElement.querySelector(".date");
+    let mauritiusTimeElement = mauritiusElement.querySelector(".time");
+    mauritiusDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+    mauritiusTimeElement.innerHTML = moment()
+      .tz("Indian/Mauritius")
+      .format("H:mm:s");
+  }
 }, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("-", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("H:mm:s")}</div>
+        </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
